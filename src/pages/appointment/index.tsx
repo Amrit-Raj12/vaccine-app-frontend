fd8b94a4afd8b4d1d8c4de4f5d6b1d5babaf8499
Amrit-Raj12 <amrit.raj1224@gmail.com>
2024-01-11 22:39:25 +0530

    Dev - added appoinment functionalities

--- src/pages/appointment/index.tsx
+++ src/pages/appointment/index.tsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import Calendar from '@/components/Callendar';
+import { useCallback, useState } from 'react';
+import appointmentService from '@/services/appointmentService';
+import useFetch from '@/hooks/useFetch';
+import Modal from '@/components/Modal';
+
+interface Event {
+  title: string;
+  start: Date;
+  end: Date;
+}
 
 const GetAppointments = () => {
+
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+
+  const getAvailableDates = useCallback(() => {
+      return appointmentService.getAvailibity();
+  }, []);
+
+  const { data, error, isLoading, isError, isSuccess } = useFetch(getAvailableDates);
+
+  console.log("Api Called",data);
+
+
+  
+  const events =  data?.map((date: string) => ({
+    title: 'Available',
+    start: new Date(date),
+    end: new Date(date),
+  }));
+
+  const handleEventClick = (event: Event) => {
+    if (event.title === 'Available') {
+      setModalVisible(true)
+      console.log('Available date clicked:', event);
+    }
+  };
+
+
   return (
     <div>
-      <h1>Appointment list</h1>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="container mx-auto">
+        <Calendar events={events} handleEventClick={handleEventClick} />
+      </div>
+    </div>
+    <Modal isOpen={modalVisible} onClose={() => { setModalVisible(false) }} >
+      <h1>Hello I am Modal</h1>
+    </Modal>
     </div>
   );
 };
