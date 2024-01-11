558a609fc479b6cf17cb08b8c4765e61dc6c5d16
Amrit-Raj12 <amrit.raj1224@gmail.com>
2024-01-11 22:38:50 +0530

    Removed appoinment component to appoinment route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,9 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Calendar from '@/components/Callendar';
-import { useCallback } from 'react';
-import appointmentService from '@/services/appointmentService';
-import useFetch from '@/hooks/useFetch';
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
-
-
-
-  const getAvailableDates = useCallback(() => {
-      return appointmentService.getAvailibity();
-  }, []);
-
-  const { data, error, isLoading, isError, isSuccess } = useFetch(getAvailableDates);
-
-  // console.log("Api Called",data);
-
-  const availableDates: Date[] = [
-    new Date(2023, 11, 23),
-    new Date(2023, 11, 24),
-    new Date(2023, 11, 25),
-    new Date(2023, 11, 26),
-    new Date(2023, 11, 27),
-    new Date(2023, 11, 28),
-    new Date(2023, 11, 30),
-    new Date(2023, 11, 31),
-    // Add more available dates as needed
-  ];
   
-  const events = availableDates.map(date => ({
-    title: 'Available',
-    start: date,
-    end: new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1),
-  }));
-
   return (
     <main
     >
-      <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="container mx-auto">
-        <Calendar events={events} />
-      </div>
-    </div>
+      <h1>Welcome to Vaccinate Site</h1>
     </main>
   )
 }
