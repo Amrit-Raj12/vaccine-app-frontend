952832eb07b951556cd939527a100897aa42c0f2
Amrit-Raj12 <amrit.raj1224@gmail.com>
2024-01-11 22:36:40 +0530

    Dev - achanged token access logic

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,8 +3,19 @@ import Header from './Header';
 import Footer from './Footer';
 import { Inter } from 'next/font/google';
 import Sidebar from './Sidebar';
+import { useSelector } from 'react-redux';
 
 const inter = Inter({ subsets: ['latin'] });
+
+interface RootState {
+  userStore: {
+    mainUser: {
+      token: {
+        accessToken: string
+      }
+    };
+  };
+}
 
 interface MyComponentProps {
   children: ReactNode; // Define the type for children
@@ -12,19 +23,15 @@ interface MyComponentProps {
 }
 
 const Layout: React.FC<MyComponentProps> = ({ children }) => {
-  const [hasToken, setHasToken] = useState<boolean>(false);
-
-  // useEffect(() => {
-  //   const token: string | null = localStorage.getItem('token');
-  //   setHasToken(!!token);
-  // }, []);
+ 
+  const {accessToken} = useSelector((state: RootState) => state.userStore.mainUser.token)
 
   return (
     <div className='flex flex-col h-screen w-full'>
       <Header />
       <div className="flex flex-1">
-        {hasToken && <Sidebar />}
-        <main className={hasToken ? 'flex-1' : 'w-full'}>
+        {accessToken && <Sidebar />}
+        <main className={accessToken ? 'flex-1' : 'w-full'}>
           {children}
         </main>
       </div>
